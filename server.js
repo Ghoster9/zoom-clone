1307334212f32c409f05f4ee1012524ec2f96def
ahhzaky <siahmadzaky@gmail.com>
2020-09-13 23:43:39 +0700

    Setup BackEnd and frontend(V)

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
-//set ejs view
-app.set("view engine", "ejs");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+app.set("view engine", "ejs"); //set ejs view
 app.use(express.static("public"));
+
+app.use("/peerjs", peerServer);
 
 // mendapatkan respon dari request '/:unieq id'
 app.get("/", (req, res) => {
@@ -13,6 +19,14 @@ app.get("/", (req, res) => {
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+//join the room (koneksi)
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(3030);
