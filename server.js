b639f2661335338685c8dcc3a02e9b2fe1f61190
ahhzaky <siahmadzaky@gmail.com>
2020-09-13 20:34:25 +0700

    Setup video on web

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+//set ejs view
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+// mendapatkan respon dari request '/:unieq id'
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 server.listen(3030);
